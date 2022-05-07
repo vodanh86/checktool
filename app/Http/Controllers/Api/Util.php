2f3084da6ed820bea38ec84a3eb7628a556fa66d
Manh Cuong <cuong@radioactive.sg>
2022-05-07 22:05:14 +0800

    add price logic

--- app/Http/Controllers/Api/Util.php
+++ app/Http/Controllers/Api/Util.php
@@ -14,8 +14,34 @@ final class Util {
             $supportedModel = SupportedModel::where('type', 'like', '%' . $request->get('type') . '%')->where('model', 'like', '%' . $request->get('model') . '%')->first();
             if ($supportedModel){
                 $price = Price::where('model_id', $supportedModel->id)->first();
+                $countComponents = 0;
+                if ($request->input('screen') != "1") $countComponents ++;
+                if ($request->input('case') != "1") $countComponents ++;
+                if ($request->input('keyboard') != "1") $countComponents ++;
+                if ($request->input('diaphragm') != "1") $countComponents ++;
+                if ($request->input('finger_print') != "1") $countComponents ++;
+                if ($request->input('sim_tray') != "1") $countComponents ++;
+                if ($request->input('home') != "1") $countComponents ++;
+                if ($request->input('face_id') != "1") $countComponents ++;
+                if ($request->input('touch_3d') != "1") $countComponents ++;
+                if ($request->input('true_tone') != "1") $countComponents ++;
+                if ($request->input('charger') != "1") $countComponents ++;
+                if ($request->input('wireless_charger') != "1") $countComponents ++;
+                if ($request->input('cellphone_wave') != "1") $countComponents ++;
+                if ($request->input('wifi') != "1") $countComponents ++;
+                if ($request->input('speaker') != "1") $countComponents ++;
+                if ($request->input('microphone') != "1") $countComponents ++;
+                if ($request->input('front_camera') != "1") $countComponents ++;
+                if ($request->input('back_camera') != "1") $countComponents ++;
+                if ($request->input('calling_sensor') != "1") $countComponents ++;
+                if ($request->input('light_sensor') != "1") $countComponents ++;
+                if ($request->input('compass_sensor') != "1") $countComponents ++;
+                if ($request->input('headphone') != "1") $countComponents ++;
                 if ($price) {
-                    return $price->level1_price;
+                    if ($countComponents == 0) return $price->level1_price;
+                    if ($countComponents == 1) return $price->level2_price;
+                    if ($countComponents == 2) return $price->level3_price;
+                    return $price->level4_price;
                 }
             }
         }
